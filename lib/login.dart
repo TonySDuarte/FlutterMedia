c0641874348a94d4fc09b5f3438d0cd1b9dc42d8
Rodrigo Ferreira <ltdanonstandard@gmail.com>
2020-09-23 15:55:25 -0300

    enviando tela de login

---
+++ lib/login.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromARGB(2, 56, 128, 255),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                style: new TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+              Divider(),
+              TextFormField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                style: new TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  labelText: "Senha",
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          )),
+        ));
+  }
+}
